878348ab1d48d0b8e06107effeb82752d0b0dca9
Jingkei Ly <jkly25@gmail.com>
2016-08-10 23:01:45 +0100

    remove unnecessary class

--- src/main/kotlin/kspark/Kspark.kt
+++ src/main/kotlin/kspark/Kspark.kt
@@ -2,7 +2,6 @@ package kspark
 
 import spark.Request
 import spark.Response
-import spark.Route
 import spark.Spark
 
 fun get(endpoint : String,
@@ -19,12 +18,10 @@ fun halt(status: Int, body: String) {
     Spark.halt(status, body)
 }
 
-private fun adapt(handler: RouteHandler.() -> Any) = RouteAdaptor(handler)
+private fun adapt(handler: RouteHandler.() -> Any) =
+        { request: Request, response: Response ->
+            handler.invoke(RouteHandler(request, response))
+        }
 
-private class RouteAdaptor(val handler: RouteHandler.() -> Any) : Route {
-    override fun handle(request: Request?, response: Response?): Any {
-        return handler.invoke(RouteHandler(request!!, response!!))
-    }
-}
 
 data class RouteHandler(val request: Request, val response: Response)
