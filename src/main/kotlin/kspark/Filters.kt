33fdfad9d5137a2ef3e68f138c4ba6749142702f
Jingkei Ly <jkly25@gmail.com>
2016-08-11 01:49:24 +0100

    add accept types

--- src/main/kotlin/kspark/Filters.kt
+++ src/main/kotlin/kspark/Filters.kt
@@ -12,6 +12,22 @@ fun after(path: String, routeHandler: KRoute.() -> Unit) {
     Spark.after(path, makeFilter(routeHandler))
 }
 
+fun before(routeHandler: KRoute.() -> Unit) {
+    Spark.before(makeFilter(routeHandler))
+}
+
+fun after(routeHandler: KRoute.() -> Unit) {
+    Spark.after(makeFilter(routeHandler))
+}
+
+fun before(path: String, acceptType: String, routeHandler: KRoute.() -> Unit) {
+    Spark.before(path, acceptType, makeFilter(routeHandler))
+}
+
+fun after(path: String, acceptType: String, routeHandler: KRoute.() -> Unit) {
+    Spark.after(path, acceptType, makeFilter(routeHandler))
+}
+
 private fun makeFilter(handle: KRoute.() -> Unit) =
         { request: Request, response: Response ->
             handle(KRoute(request, response))
