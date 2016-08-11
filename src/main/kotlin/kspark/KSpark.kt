1d33327dc970118b2271dd31c16fc2d667457131
Jingkei Ly <jkly25@gmail.com>
2016-08-11 01:35:17 +0100

    added additional verbs

--- src/main/kotlin/kspark/KSpark.kt
+++ src/main/kotlin/kspark/KSpark.kt
@@ -16,6 +16,34 @@ fun halt(status: Int, body: String) {
     Spark.halt(status, body)
 }
 
+fun put(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.put(path, adapt(routeHandler))
+}
+
+fun patch(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.patch(path, adapt(routeHandler))
+}
+
+fun delete(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.delete(path, adapt(routeHandler))
+}
+
+fun head(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.head(path, adapt(routeHandler))
+}
+
+fun trace(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.trace(path, adapt(routeHandler))
+}
+
+fun connect(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.connect(path, adapt(routeHandler))
+}
+
+fun options(path: String, routeHandler: KRoute.() -> Any) {
+    Spark.options(path, adapt(routeHandler))
+}
+
 private fun adapt(handle: KRoute.() -> Any) =
         { request: Request, response: Response ->
             handle(KRoute(request, response))
