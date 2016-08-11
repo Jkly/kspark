ca3d0249a188573071ca4292c814bdbb71d8948e
Jingkei Ly <jkly25@gmail.com>
2016-08-11 01:23:02 +0100

    add example for receiving json

--- src/main/kotlin/example/JsonExample.kt
+++ src/main/kotlin/example/JsonExample.kt
@@ -1,6 +1,7 @@
 package example
 
 import kspark.*
+import kspark.extension.fromJson
 import kspark.extension.toJson
 
 data class FooBar(val foo:String, val bar:String)
@@ -9,5 +10,10 @@ fun main(args: Array<String>) {
     get("/example.json") {
         response.type("application/json")
         FooBar("foo", "bar").toJson()
+    }
+
+    post("/example.json") {
+        val fooBar:FooBar = request.body().fromJson()
+        "got ${fooBar.foo} ${fooBar.bar}"
     }
 }
