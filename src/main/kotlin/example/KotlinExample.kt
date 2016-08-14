0992ebb1306f26b2732ad5bbe3e9df687520a886
Jingkei Ly <jkly25@gmail.com>
2016-08-14 02:53:32 +0100

    fix example to use kotlin string formatting

--- src/main/kotlin/example/KotlinExample.kt
+++ src/main/kotlin/example/KotlinExample.kt
@@ -7,18 +7,18 @@ import spark.Spark.halt
 fun main(args: Array<String>) {
     get("/hello") { "Hello World!" }
 
-    post("/hello") { "Hello World: " + request.body() }
+    post("/hello") { "Hello World: ${request.body()}"  }
 
     get("/private") {
         response.status(401)
         "Go Away!!!"
     }
 
-    get("/users/:name") { "Selected user: " + request.params(":name") }
+    get("/users/:name") { "Selected user: ${request.params(":name")}" }
 
     get("/news/:section") {
         response.type("text/xml")
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>"
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>${request.params("section")}</news>"
     }
 
     get("/protected") {
